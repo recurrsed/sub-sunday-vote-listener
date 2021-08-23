a8b7b37a7f0581210615352ce5d40acfd3e59bcb
Emir Maljanovic <emir.maljanovic@gmail.com>
2021-08-23 21:27:10 +0200

    Group votes by week number; Fixed app reinit

--- saveToFirebase.js
+++ saveToFirebase.js
@@ -1,18 +1,32 @@
 const admin = require("firebase-admin");
 const serviceAccount = require("./firebase.json");
 
+function getWeekNumber(d = new Date()) {
+  d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
+  d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
+  d.setMilliseconds(0);
+
+  const yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
+  const weekNo = Math.ceil((((d - yearStart) / 86400000) + 1) / 7);
+
+  return weekNo;
+}
+
 const saveToFirebase = (chatData) => {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://twitchsniffer-25e09.firebaseio.com"
-  });
-  const rootRef = admin.database().ref('/chat/ZpcM8kKeNEOnH8ovBniT');
+  if (!admin.apps.length) {
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+      databaseURL: "https://twitchsniffer-25e09.firebaseio.com"
+    });
+  }
+
+  const thisWeekRef = admin.database().ref(`/chat/${getWeekNumber()}`);
   const promises = [];
 
   console.log('Firebase initialized...');
 
   (chatData || []).forEach((item) => {
-    promises.push(rootRef.child(item.userstate['user-id']).set(item));
+    promises.push(thisWeekRef.child(item.userstate['user-id']).set(item));
   });
 
   return Promise.all(promises)
